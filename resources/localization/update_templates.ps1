f285e7ed2d6fba504cf18aac203e8e91b372407d
Nikolas Dahn <nikolas.dahn@gmail.com>
2026-04-22 13:35:47 +0200

    Reverted build script to cmd, easier with miniforge

--- resources/localization/update_templates.ps1
+++
@@ -1,5 +0,0 @@
-& "$env:USERPROFILE\miniforge3\Scripts\activate.ps1"
-conda activate base
-pip install Babel
-pybabel extract --keyword=µ:1,1t --keyword=µ:1,2c,2t --output-file=template.pot --sort-by-file --input-dirs=../../yonder/gui
-pybabel update --domain=yonder --input-file=template.pot --output-dir=.
